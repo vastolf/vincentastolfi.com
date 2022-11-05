ab7c79fbae19d4486735610c14ac332bec2e4ac6
vastolf <astolfivincent@gmail.com>
2022-11-05 13:11:02 -0600

    Rename calculateAge to calculateYearsSince

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -4,7 +4,7 @@ import './styles.css'
 
 const Bio = () => {
     
-    const calculateAge = (dateString: string) => {
+    const calculateYearsSince = (dateString: string) => {
         var date = new Date(dateString);
         // Get month difference 
         var monthsDiff = Date.now() - date.getTime();
@@ -24,8 +24,8 @@ const Bio = () => {
                 <img className="bio__image" src="/me.png" alt="A small photo of Vincent" />
             }
         >
-            <p>I am a {calculateAge("01/17/1996")}-year-old Developer originally from San Diego, CA, currently based in Costa Rica. Mother tongue is English, but I'm also fluent in Spanish.</p>
-            <p>Working remote for the past {calculateAge("12/01/2015")}+ years. Love learning new technologies &amp; solving problems I've never encountered before.</p>
+            <p>I am a {calculateYearsSince("01/17/1996")}-year-old Developer originally from San Diego, CA, currently based in Costa Rica. Mother tongue is English, but I'm also fluent in Spanish.</p>
+            <p>Working remote for the past {calculateYearsSince("12/01/2015")}+ years. Love learning new technologies &amp; solving problems I've never encountered before.</p>
         </Section>
     )
 }
