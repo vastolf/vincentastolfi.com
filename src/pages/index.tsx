388ddcbf31f7787d8b63e1cdf404758e23a15c06
vastolf <astolfivincent@gmail.com>
2022-11-07 10:10:00 -0600

    Simplifying check for invalid fields, contact form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import React from "react"
 import type { HeadFC } from "gatsby"
 import Layout from "../components/Layout/Layout"
-import Header from "../components/Header/Header"
 import Marquee from "../components/Marquee/Marquee"
 import Bio from "../components/Bio/Bio"
 import Experience from "../components/Experience/Experience"
 import References from "../components/References/References"
 import Contact from "../components/Contact/Contact"
-import Footer from "../components/Footer/Footer"
 
 const IndexPage = () => {
   return (
