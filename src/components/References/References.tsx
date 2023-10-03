dac6af2b4d75d8fa8b87dfa7c7df4b539925a602
vastolf <astolfivincent@gmail.com>
2023-10-03 17:29:11 -0600

    Change references text presently -> shortly

--- src/components/References/References.tsx
+++ src/components/References/References.tsx
@@ -13,7 +13,7 @@ const References = () => {
             }
         >
             <p>More references are available upon request. You can feel free to contact <a href="https://desarol.com">Desarol</a> with any questions regarding my recent work history.</p>
-            <p>If you have any questions or need to reach me for any reason, please use the contact form below; I'll be responding to you presently.</p>
+            <p>If you have any questions or need to reach me for any reason, please use the contact form below, and I'll be responding to you shortly.</p>
         </Section>
     )
 }
